1b3201230181fa130fe338748fc57f193b646b8b
Stanislav Shchelokov <stanislavschelokov@gmail.com>
2024-12-12 02:23:29 +0200

    created logo component

---
+++ components/main-logo/main-logo.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <nuxt-link
+        to="/"
+        class="main-logo"
+    >
+        <img
+            src="~/assets/svg/logo/inst_room_logo.svg"
+            alt="Inst Room Logo"
+        />
+    </nuxt-link>
+</template>
+<script setup lang="ts">
+import './main-logo.scss'
+</script>
+<style lang="scss"></style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,12 @@
     <main-container
         >Main page
         <main-button>ПЕРЕЙТИ К НОВОСТЯМ</main-button>
+        <main-logo />
     </main-container>
 </template>
 
 <script setup lang="ts">
 import mainContainer from '~/components/main-container/main-container.vue'
 import mainButton from '~/components/UI/main-button/main-button.vue'
+import mainLogo from '~/components/main-logo/main-logo.vue'
 </script>
